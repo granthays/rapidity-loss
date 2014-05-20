802bf7f4e1a53fda74eb9b0c9b9972f114db9805
Grant Hays <granthays@gmail.com>
2014-05-20 14:44:42 -0500

    Starting the FitParMin code to find best-fit parameters

---
+++ FitParMin.cxx
@@ -0,0 +1,15 @@
+/*********************************************************************************
+ * 			Finding Best Fit Parameters of Rapidity Loss Function
+ *
+ * Title:		FitFind.cxx
+ * Author:		Grant Hays
+ * Date: 		28 Mar. 2014
+ * Description:	This is a ROOT macro to find the best-fit parameters of the 
+ *				rapidity loss function estimated by Chris A. Minimization
+ *				techniques will be used from MINUIT, MINUIT2, MINOS, or MINGRAD.
+ *********************************************************************************/
+ 
+void FitParMin()
+{
+	
+}
