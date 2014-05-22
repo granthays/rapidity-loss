1b8228fd825a24694331c8b9e5a27f62657a5a97
Grant Hays <granthays@gmail.com>
2014-05-22 00:01:21 -0500

    New version of TreeHouse works and wrote 158GeVP.root

--- TreeHouse.cxx
+++ TreeHouse.cxx
@@ -1,76 +1,33 @@
-// Read data (CERN staff) from an ascii file and create a root file with a Tree.
-// see also a variant in staff.C
-// Author: Rene Brun
-   
-TFile *TreeHouse(Int_t get=0, Int_t print=1) {
+// Reads a simple data file 
+#include <TFile.h> 
+#include <TTree.h> 
+#include <string> 
+#include <iostream> 
+using namespace std; 
+void TreeHouse2() { 
+	// Location of the directory for input and output files 
+	string dirName = "D:/Users/Grant/Documents/GitHub/rapidity-loss/" ; 
+	
+	// Name of the input and output files 
+	string inFileName = dirName + "158GeVP.txt" ; 
+	string outFileName = dirName + "Tree158GeVP.root" ; 
+	
+	// Create a tree to store the data 
+	TTree *tree = new TTree("tree158GeVP","158 GeV (Positive)"); 
 
-	Double_t = y;
-	Double_t = dy;
-	Double_t = edy;
-   Int_t           Category;
-   UInt_t          Flag;
-   Int_t           Age;
-   Int_t           Service;
-   Int_t           Children;
-   Int_t           Grade;
-   Int_t           Step;
-   Int_t           Hrweek;
-   Int_t           Cost;
-   Char_t          Division[4];
-   Char_t          Nation[3];
-
-   //The input file cern.dat is a copy of the CERN staff data base
-   //from 1988
-   TString filename = "TreeHouse.root";
-   TString dir = gSystem->UnixPathName(gInterpreter->GetCurrentMacroName());
-   dir.ReplaceAll("TreeHouse.cxx","");
-   dir.ReplaceAll("/./","/");
-   FILE *fp = fopen(Form("%158GeVP.txt",dir.Data()),"r");
-
-   TFile *hfile = 0;
-   if (get) {
-      // if the argument get =1 return the file "TreeHouse.root"
-      // if the file does not exist, it is created
-      if (!gSystem->AccessPathName(dir+"TreeHouse.root",kFileExists)) {
-         hfile = TFile::Open(dir+"TreeHouse.root"); //in $ROOTSYS/tutorials/tree
-         if (hfile) return hfile;
-      }
-      //otherwise try $PWD/TreeHouse.root
-      if (!gSystem->AccessPathName("TreeHouse.root",kFileExists)) {
-         hfile = TFile::Open("TreeHouse.root"); //in current dir
-         if (hfile) return hfile;
-      }
-   }
-   //no TreeHouse.root file found. Must generate it !
-   //generate TreeHouse.root in $ROOTSYS/tutorials/tree if we have write access
-   if (!gSystem->AccessPathName(dir,kWritePermission)) {
-      filename = dir+"TreeHouse.root";
-   } else if (!gSystem->AccessPathName(".",kWritePermission)) {
-      //otherwise generate TreeHouse.root in the current directory
-   } else {
-      printf("you must run the script in a directory with write access\n");
-      return 0;
-   }
-   hfile = TFile::Open(filename,"RECREATE");
-   TTree *tree = new TTree("T","158 GeV (Positive)");
-   tree->Branch("y",&y,"y/D");
-   tree->Branch("dy",&dy,"dy/D");
-   tree->Branch("edy",&edy,"edy/D");
-
-   char line[80];
-   while (fgets(line,80,fp)) {
-      sscanf(&line[0],"%d %d %d",&y,&dy,&edy);
-      tree->Fill();
-   }
-   if (print) tree->Print();
-   tree->Write();
-
-   fclose(fp);
-   delete hfile;
-   if (get) {
-      //we come here when the script is executed outside $ROOTSYS/tutorials/tree
-      hfile = TFile::Open(filename);
-      return hfile;
-   }
-   return 0;
-}
+	// Open the output file to write to 
+	TFile *fout = new TFile(outFileName.c_str(),"RECREATE"); 
+	fout->cd(); 
+	
+	// Read in the data from the text file 
+	int nentries = tree->ReadFile(inFileName.c_str(), 
+		"y/D:dy/D:edy/D"); 
+		cout << "Read " << nentries << " entries from the input file " << 
+		inFileName << endl; 
+	// Write the ROOT tree to output file 
+	tree->Write(); 
+	fout->Close(); 
+	// Cleanup 
+	delete tree; 
+	delete fout; 
+} 
