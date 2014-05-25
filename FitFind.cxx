6c370c138d27c2e614a4437938d632a02233406a
Grant Hays <granthays@gmail.com>
2014-05-25 15:51:49 -0500

    Added some stuff

--- FitFind.cxx
+++ FitFind.cxx
@@ -10,29 +10,34 @@
  *********************************************************************************/
 void FitFind(){
 
-	double grphMin = 0.;
-	double grphMax = 2.9;
+	// The range of the fit.
+	double fitMin = 0.4;
+	double fitMax = 3.6;
 	
-	double xMin = -2.;
-	double xMax = 4.;
+	// The range of the X-axis on the printed graph
+	double xMin = 0.;
+	double xMax = 5.;
 	
+	// The range of the Y-axis on the printed graph
 	double yMin = 0.;
 	double yMax = 0.4;
 	
-	double p0 = 0.04;
-	double p1 = 1.13;
+	// Initial guesses for the parameters
+	double p0 = 0.0093;
+	double p1 = 1.45;
 	
 	// Sets the function of fit to accommodate a half dataset or a full one
-	//	true  =  Apply fit for half dataset i.e. x >= 0
-	//	false =  Apply fit for full dataset
+	//		true  =  Apply fit for half dataset i.e. x >= 0
+	//		false =  Apply fit for full dataset
 	bool isHalf = true;
 	
 	// Determine which parameters to fix
-	//	0 = No Parameters Fixed, both vary
-	//	1 = Fix the first parameter i.e. p0
-	//	2 = Fix the second parameter i.e. p1
-	//	3 = Fix both parameters
-	int fixPar = 1;
+	//		0 = No Parameters Fixed, both vary
+	//		1 = Fix the first parameter i.e. p0
+	//		2 = Fix the second parameter i.e. p1
+	//		3 = Fix both parameters
+	//		4 = No parameters fixed, but both have limits
+	int fixPar = 0;
 	
 /*********************************************************************************/
 	
@@ -45,11 +50,11 @@ void FitFind(){
 	gStyle->SetOptFit();
 	
 	// Creating and filling the TGraphErrors
-	TGraphErrors *grph = new TGraphErrors("./158GeVP.txt","%lg %lg %lg");
+	TGraphErrors *grph = new TGraphErrors("./200GeVP.dat","%lg %lg %lg");
 	
 	// Set titles for the graph and it's axes
 	grph->SetTitle(
-		"158 GeV (Positive);
+		"200 GeV (Positive);
 		y';
 		dN/dy'");
 	// Set plot marker style to filled squares
@@ -65,12 +70,22 @@ void FitFind(){
 	//		P = The current marker is plotted at each point
 	grph->Draw("AP");
 	
-	// This is the fit function for the whole range of data. Gaussian + Landau
+	
+	// This is the fit function found by Chris Appier. If isHalf is true, then
+	//		the function is only for the positive half: 
+	//				
+	//				p0 * Exp(x/p1)
+	//		
+	//		Otherwise the function is for both sides:
+	//
+	//				p0 * ( Exp(x/p1) + Exp(-x/p1) )
+	//
 	if(isHalf){
-		TF1 *total = new TF1("total","[0]*(TMath::Exp(x/[1]))",grphMin,grphMax);
+		TF1 *total = new TF1("total","[0]*(TMath::Exp(x/[1]))",fitMin,fitMax);
 	} else {
-		TF1 *total = new TF1("total","[0]*(TMath::Exp(x/[1]) + TMath::Exp(-x/[1]))",grphMin,grphMax);
+		TF1 *total = new TF1("total","[0]*(TMath::Exp(x/[1]) + TMath::Exp(-x/[1]))",fitMin,fitMax);
 	}
+	
 	
 	// Set the fit line color to red, for some reason (I should probably change this)
 	total->SetLineColor(kAzure);
@@ -85,24 +100,22 @@ void FitFind(){
 	} else if(fixPar == 2) {
 		total->SetParameter(0,p0);
 		total->FixParameter(1,p1);
-	} else {
+	} else if(fixPar == 3){
 		total->FixParameter(0,p0);
 		total->FixParameter(1,p1);
+	} else {
+		total->SetParameter(0,p0);
+		total->SetParLimits(0,p0-0.01869,p0+0.01869);
+		total->SetParameter(1,p1);
+		total->SetParLimits(1,1,2);
 	}
 	
+/****************************************/
+	// Fitting the entire range with the fit function
+	grph->Fit(total,"WVEM");
+/****************************************/
 	
 	
-	/*
-	// Using the retrieved parameter info to set the parameters for the full fit function
-	total->FixParameter(0,p0);
-	//total->FixParameter(1,p1);
-	total->SetParameter(1,p1);
-	*/
-	
-	
-	
-	// Fitting the entire range with the fit amalgamation
-	grph->Fit(total);
 	
 	// Retrieve and print the number of parameters in the function
 	int numParam = total->GetNpar();
@@ -117,14 +130,19 @@ void FitFind(){
 	Double_t *parErrors = total->GetParErrors();
 	
 	// Create a function for the mean integral
-	TF1 *total2 = new TF1("total2","total * x",grphMin,grphMax);
+	TF1 *total2 = new TF1("total2","total * x",fitMin,fitMax);
 	
 	// Compute integral for bottom of the expectation value function
-	Double_t integral1 = total->Integral(grphMin,grphMax);
+	Double_t integral1 = total->Integral(fitMin,fitMax);
 	// Compute error of the integral
-	Double_t integral1Error = total->IntegralError(grphMin,grphMax,parErrors,covMatrix.GetMatrixArray());
+	Double_t integral1Error = total->IntegralError(fitMin,fitMax,parErrors,covMatrix.GetMatrixArray());
 	// Compute integral for top of the expectation value function
-	Double_t integral2 = total2->Integral(grphMin,grphMax);
+	Double_t integral2 = total2->Integral(fitMin,fitMax);
+	// Compute error of the integral
+	Double_t integral2Error = total2->IntegralError(fitMin,fitMax,parErrors,covMatrix.GetMatrixArray());
+	
+	cout << "\n\n\tError of first integral:\t" << integral1Error;
+	cout << "\n\tError of second integral:\t" << integral2Error; 
 	
 	// Print integrals and the expectation value
 	//cout << "\n\n\tIntegral y * dN/dy:\t" << integral2 << endl << endl;
